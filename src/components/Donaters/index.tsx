ee22f75f81f7c43e2d40361741465538b6ddc4de
Jefferson Mendonca <jefferson.mendonca@theos.com.br>
2024-12-22 12:17:48 -0300

    home: home page implementation and Donaters component

---
+++ src/components/Donaters/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./style.module.scss";
+
+export function Donaters() {
+
+    return (
+        <div className={styles.donaters}>
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuario 1"/>
+        </div>
+    )
+}
